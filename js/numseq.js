1056f7880c59f36e486a7ac3d82177e34e26dcc9
georgi57 <georgi.olentsenko@gmail.com>
2021-09-30 11:32:42 +0100

    Shrunk variable names for convenience

--- js/numseq.js
+++ js/numseq.js
@@ -4,14 +4,13 @@ Started 2021-09-27
 */
 
 // State of the app
-var check_sequence = false;
+var check_seq = false;
 var congratulate = false;
-var increment_length = true;
+var inc_len = true;
 
-var number_sequence = "";
-var number_sequence_length = 4;
-var number_sequence_index = 20;
-var number_of_digits_tickbox = document.querySelector('input[id="number_of_digits"]');
+var numseq = "";
+var numseq_length = 4;
+var numseq_index = 20;
 
 // Initialize new SpeechSynthesisUtterance object
 let speech = new SpeechSynthesisUtterance();
@@ -19,32 +18,33 @@ var speaking = false;
 var speech_length = 0;
 
 // Generate a new number sequence for the memory training
-function generate_number_sequence() {
-	number_sequence = "";
-	for (let i = 0; i < number_sequence_length; i++) {
-		number_sequence = number_sequence + Math.floor(Math.random() * 10);
+function generate_numseq() {
+	numseq = "";
+	for (let i = 0; i < numseq_length; i++) {
+		numseq = numseq + Math.floor(Math.random() * 10);
 	}
-	check_sequence = true;
+	check_seq = true;
 	
 	speech_length = 0;
-	number_sequence_index = 0;
+	numseq_index = 0;
 	if (document.getElementById("number_of_digits_checkbox").checked) {
-		document.getElementById("number_of_digits").innerHTML = number_sequence_length;
+		document.getElementById("number_of_digits").innerHTML = numseq_length;
 	}
-	speech.text = number_sequence[number_sequence_index];
+	speech.text = numseq[numseq_index];
 	speaking = true;
 	window.speechSynthesis.speak(speech);
 }
 
 speech.addEventListener('end', function(event) {
   speech_length += event.elapsedTime;
-  number_sequence_index += 1;
+  numseq_index += 1;
   
-  if (number_sequence_index >= number_sequence_length) {
+  if (numseq_index >= numseq_length) {
 		speaking = false;
-		console.log('It took ' + speech_length + ' seconds to utter the number sequence');
+		console.log('It took ' + speech_length +
+		' seconds to utter the number sequence');
   } else {
-		speech.text = number_sequence[number_sequence_index];
+		speech.text = numseq[numseq_index];
 		speaking = true;
 		window.speechSynthesis.speak(speech);
   }
@@ -54,7 +54,7 @@ speech.addEventListener('end', function(event) {
 function settings() {
 	//  Set whether to see or not the number of digits
 	if (document.getElementById("number_of_digits_checkbox").checked) {
-		document.getElementById("number_of_digits").innerHTML = number_sequence_length;
+		document.getElementById("number_of_digits").innerHTML = numseq_length;
 	} else {
 		document.getElementById("number_of_digits").innerHTML = "X";
 	}
@@ -67,51 +67,51 @@ function settings() {
 	}
 	
 	// Set whether to increment sequence length on success
-	if (document.getElementById("increment_length").checked) {
-		increment_length = true;
+	if (document.getElementById("inc_len").checked) {
+		inc_len = true;
 	} else {
-		increment_length = false;
+		inc_len = false;
 	}
 }
 
 // Function to check the sequence you enter
-function check_number_sequence() {
+function check_numseq() {
 	// Check whether test is in progress
-	if (check_sequence) {
+	if (check_seq) {
 		// Get the input sequence
-		var number_sequence_input = document.getElementById("number_sequence_input").value;
-		console.log(number_sequence + " vs " + number_sequence_input + " (last one yours)");
+		var numseq_input = document.getElementById("numseq_input").value;
+		console.log(numseq + " vs " + numseq_input + " (last one yours)");
 		// Show differences in sequence if any
-		document.getElementById('correct_number_sequence').innerHTML = '';
-		for (var i = 0; i < number_sequence_length; i++) {
-			if (number_sequence[i]==number_sequence_input[i]) {
-			document.getElementById('correct_number_sequence').innerHTML +=
-				'<a style="background-color: #98e698">'+number_sequence[i]+'</a>';
+		document.getElementById('correct_numseq').innerHTML = '';
+		for (var i = 0; i < numseq_length; i++) {
+			if (numseq[i]==numseq_input[i]) {
+			document.getElementById('correct_numseq').innerHTML +=
+				'<a style="background-color: #98e698">'+numseq[i]+'</a>';
 			} else {
-				document.getElementById('correct_number_sequence').innerHTML +=
-				'<a style="background-color: #ff3333">'+number_sequence[i]+'</a>';
+				document.getElementById('correct_numseq').innerHTML +=
+				'<a style="background-color: #ff3333">'+numseq[i]+'</a>';
 			}
 		}
 		// Compare the generated and input sequence
-		if (number_sequence == number_sequence_input) {
+		if (numseq == numseq_input) {
 			console.log('Correct sequence!');
 			if (congratulate) {
 				speech.text = "Well done!";
 				window.speechSynthesis.speak(speech);
 			}
 			// Increment sequence length if set to do so
-			if (increment_length) {
-				number_sequence_length += 1;
+			if (inc_len) {
+				numseq_length += 1;
 			}
 		}
-		document.getElementById("button_start_stop").innerHTML = "Listen";
-		check_sequence = false;
+		document.getElementById("btn_ctrl").innerHTML = "Listen";
+		check_seq = false;
 	} else {
 		// Clear form input
-		document.getElementById("number_sequence_input").value = "";
-		document.getElementById("correct_number_sequence").innerHTML = "";
-		document.getElementById("button_start_stop").innerHTML = "Check";
+		document.getElementById("numseq_input").value = "";
+		document.getElementById("correct_numseq").innerHTML = "";
+		document.getElementById("btn_ctrl").innerHTML = "Check";
 		// Start anew if sequence is already checked or none there
-		generate_number_sequence();
+		generate_numseq();
 	}
 }
